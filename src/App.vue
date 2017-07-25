549f98a39dadbf3d26660e344ca032377a517c4c
kuma <kaito@nihonn.net>
2017-07-25 08:54:30 +0800

    feat: add the selected window to identify

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,19 @@
       <left-menu :data="shuju"></left-menu>
     </el-col>
     <el-col :span="20" class="H100 main">
-      <com-middle class="dialog" window="full" name="Hello"></com-middle>
-      <com-middle class="dialog" window="small" name="Hello"></com-middle>
+      <com-middle window="full" name="Hello"></com-middle>
+      <template v-for="(item, index) in dialog">
+        <com-middle
+          window="small"
+          :index="index"
+          :name="item.name"
+          :title="item.title"
+          :zIndex="item.zIndex"
+          :checkedClass="item.checked"
+          @com-close="comClose"
+          @com-checked="comChecked"
+        ></com-middle>
+      </template>
     </el-col>
   </el-row>
 </template>
@@ -18,6 +29,11 @@ export default {
   data () {
     return {
       /* eslint-disable */
+      dialog: [
+        {name: 'Hello', title: '您好', zIndex: 0, checked: false},
+        {name: 'Hello', title: '测试', zIndex: 1, checked: false},
+        {name: 'Hello', title: '测试2', zIndex: 2, checked: true}
+      ],
       shuju: [
         {name: '栏目1', icon: 'a', winProperty: 'a1', item: [
           {name: '二级1', icon: 'a', winProperty: 'b1', item: [
@@ -30,10 +46,28 @@ export default {
           {name: '二级2', icon: 'a', winProperty: 'b2', item: null},
           {name: '二级3', icon: 'a', winProperty: 'b3', item: null}
         ]},
-        {name: '栏目2', icon: 'a', winProperty: 'a2', item: null},
+        {name: '管理', icon: 'a', winProperty: 'a2', item: null},
         {name: '栏目3', icon: 'a', winProperty: 'a3', item: null}
       ]
       /* eslint-enable */
+    }
+  },
+  methods: {
+    /**
+     * 关闭指定组件窗口
+     * @param  {string} name 组件名
+     */
+    comClose: function (index) { // 关闭组件
+      this.dialog.splice(index, 1)
+    },
+    comChecked: function (index) {
+      let leng = this.dialog.length
+      for (let i = 0; i < leng; i++) {
+        if (this.dialog[i].zIndex > this.dialog[index].zIndex) this.dialog[i].zIndex--
+        this.dialog[i].checked = false
+      }
+      this.dialog[index].zIndex = leng - 1
+      this.dialog[index].checked = true
     }
   },
   components: {leftMenu}
--- src/components/com-middle.vue
+++ src/components/com-middle.vue
@@ -1,35 +1,49 @@
 <template>
-  <div class="wrapper" :class="window" :style="offset">
+  <div class="wrapper" :class="window" :style="[{zIndex: zIndex}, offset]" @mousedown="checked">
     <div class="dialog_header"
+      :class="checkedClass ? 'checked' : ''"
       v-if="window !== 'full'"
       v-drag="positionUpdate"
-      >流量分析</div>
+    >
+      {{title}}<i class="el-icon-close" @click="close"></i>
+    </div>
     <component :is="com"></component>
   </div>
 </template>
 <script>
 /**
- * dialog组件
- * @window [string] 窗口大小 tiny/small(默认)/large/full
- * @name [string] 调用组件名
+ * 组件调用的中间件
+ * @param {number} index 组件标识
+ * @param {string} window 窗口大小 tiny/small(默认)/large/full
+ * @param {string} name 调用组件名
+ * @param {string} title 窗口标题
+ * @param {number} zIndex 窗口层级
+ * @param {boolean} checkedClass 顶层样式判断
  */
 export default {
+  props: ['index', 'window', 'name', 'title', 'zIndex', 'checkedClass'],
   data () {
     return {
-      offset: ''
-    }
-  },
-  methods: {
-    positionUpdate: function (val) {
-      this.offset = {
-        top: val.top + 'px',
-        left: val.left + 'px'
-      }
+      offset: '' // 窗口坐标值
     }
   },
   computed: {
     com: function () { // 调用组件
       return require(`./${this.name}`)
+    }
+  },
+  methods: {
+    positionUpdate: function (val) { // 更新窗口坐标
+      this.offset = {
+        top: val.top + 'px',
+        left: val.left + 'px'
+      }
+    },
+    close: function () { // 通知父级关闭本窗口组件
+      this.$emit('com-close', this.index)
+    },
+    checked: function () { // 通知升级至顶层
+      this.$emit('com-checked', this.index)
     }
   },
   directives: {
@@ -57,8 +71,7 @@ export default {
         }
       }
     }
-  },
-  props: ['window', 'name']
+  }
 }
 </script>
 <style lang="less" scoped>
@@ -69,9 +82,22 @@ export default {
     background: #FFF;
   }
   .dialog_header{
-    background: red;
+    @height: 30px;
+    padding: 0 5px;
+    height: @height;
+    line-height: @height;
+    background: #58B7FF;
+    color: #FFF;
     cursor: move;
+    &>i{
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      font-size: 10px;
+      cursor: pointer;
+    }
   }
+  .checked{background: #1D8CE0;}
   .full{
     .W100;
     .H100;
